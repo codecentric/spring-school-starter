3f125324ed240d4eec3b4e8ef8b61d43806e59d7
Ansgar Schulte <ansgar.schulte@codecentric.de>
2021-02-17 16:15:00 +0100

    EXERCISE: 09_spring_rest_server

---
+++ 09_spring_rest_server/src/main/java/inc/monster/app/user/domain/User.java
@@ -0,0 +1,19 @@
+package inc.monster.app.user.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@Data
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private String email;
+}
---
+++ 09_spring_rest_server/src/test/java/inc/monster/app/hello/resource/HelloControllerTest.java
@@ -0,0 +1,47 @@
+package inc.monster.app.hello.resource;
+
+import inc.monster.app.hello.controller.HelloController;
+import java.util.Collections;
+import java.util.Map;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello_Accept_JSON() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Hello World!")));
+    }
+
+    @Test
+    public void getHello_Accept_XML() throws Exception {
+        Map<String, String> NS =
+                Collections.singletonMap("ns", "http://monster.inc/hello");
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_XML))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_XML))
+                .andExpect(MockMvcResultMatchers.xpath(
+                        "/ns:hello/message", NS)
+                        .string("Hello World!"));
+    }
+}

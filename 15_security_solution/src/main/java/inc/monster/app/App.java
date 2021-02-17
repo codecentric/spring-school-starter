ad0d523a6e876201b976f5b559a33ebde73d5948
Ansgar Schulte <ansgar.schulte@codecentric.de>
2021-02-17 16:15:04 +0100

    SOLUTION-COMPLETE: 15_security_solution

--- 15_security_solution/src/main/java/inc/monster/app/App.java
+++ 15_security_solution/src/main/java/inc/monster/app/App.java
@@ -2,12 +2,32 @@ package inc.monster.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class App {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
+	@Bean
+	public UserDetailsService userDetailsService() {
+		User.UserBuilder users = User.withDefaultPasswordEncoder();
+		UserDetails user = users
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+		UserDetails admin = users
+				.username("admin")
+				.password("password")
+				.roles("USER", "ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(user, admin);
+	}
 }
--- 15_security_solution/src/main/java/inc/monster/app/config/SecurityConfiguration.java
+++ 15_security_solution/src/main/java/inc/monster/app/config/SecurityConfiguration.java
@@ -1,15 +1,32 @@
 package inc.monster.app.config;
 
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = false)
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers("/", "/index", "/login", "/css/**", "/images/**").permitAll()
+                        .mvcMatchers("/car/edit").hasRole("ADMIN")
+                        .anyRequest().hasRole("USER")
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .successForwardUrl("/car/index")
+                        .failureUrl("/login-error")
+                )
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessUrl("/")
+                );
     }
 
 }
